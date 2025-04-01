8ad5b9f3897cb4ff47535daeeed4203cfd6172f7
amelia-dru <ameliadruzba2004@gmail.com>
2025-04-01 19:10:54 +0200

    Update and rename dane.py to model.py

--- model.py
+++ model.py
@@ -1,16 +1,22 @@
 #bibliotekarz
 import random
 
+id_user = 0
+id_book = 0
+
 class Ksiazka:
     def __init__(self, tytul, autor, rok:int, ilosc:int):
+        global id_book
+        id_book += 1
         self.tytul = tytul
         self.autor = autor
         self.rok = rok
         self.ilosc = ilosc
-        self.dostepnosc = True
+        self.dostepnosc = ilosc > 0
+        self.id = id_book
 
     def szczegoly(self):
-        return f'tytul: {self.tytul}, autor: {self.autor}, rok: {self.rok}'
+        return f'tytul: {self.tytul}, autor: {self.autor}, rok: {self.rok}, id: {self.id}, ilosc: {self.ilosc}'
 
 
 lista = [
@@ -33,10 +39,17 @@ lista = [
 
 class Uzytkownik():
     def __init__(self, imie, nazwisko, wiek:int):
+        global id_user 
+        id_user += 1
         self.imie = imie
         self.nazwisko = nazwisko
         self.wiek = wiek
         self.lista_ksiazek = []
+        self.id = id_user
+    
+    def szczegoly(self):
+        return f'imie i nazwisko: {self.imie} {self.nazwisko}, wiek: {self.wiek}, lista ksiazek {self.lista_ksiazek}, id: {self.id}'
+
 
 uzytkownicy = [
     Uzytkownik("Jan", "Kowalski", 25),
@@ -50,5 +63,3 @@ uzytkownicy = [
     Uzytkownik("Michał", "Szymański", 45),
     Uzytkownik("Aleksandra", "Woźniak", 29)
 ]
-
-
